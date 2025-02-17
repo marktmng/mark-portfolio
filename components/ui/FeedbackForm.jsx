39f7fff17645a916e5d3581bbfc24d480d5ad2b9
Mark Tmng <mailsareaccepting@gmail.com>
2025-02-17 17:47:27 +1300

    added more icons to skills

--- components/ui/FeedbackForm.jsx
+++ components/ui/FeedbackForm.jsx
@@ -15,26 +15,26 @@ const FeedbackForm = ({ setIsOpen }) => {
   }, [setIsOpen]);
 
   return (
-    <div className="fixed inset-0 bg-black bg-opacity-50 flex justify-center items-center z-50">
+    <div className="fixed inset-0 bg-black bg-opacity-70 flex justify-center items-center z-50">
       <div className="bg-white p-6 rounded-lg shadow-lg w-[400px] relative">
         <button
           onClick={() => setIsOpen(false)}
-          className="absolute top-3 right-3 text-gray-500 hover:text-gray-700"
+          className="absolute top-3 right-3 bg-red-300 hover:bg-red-500 text-red-500 hover:text-white rounded-[5px]"
         >
-          ✕
+          [ ✕ ]
         </button>
-        <h2 className="text-xl font-semibold mb-4">Feedback Form</h2>
-        <p className="text-gray-600">
+        <h2 className="text-xl font-semibold mb-4 text-[#02a312]">Feedback</h2>
+        <p className="text-[#02a312]">
           We value your feedback. Please fill out the form below:
         </p>
         <textarea
-          className="w-full border p-2 mt-4"
+          className="w-full border p-2 mt-4 bg-[#24292f]/10 text-black border-[#02a312]/50"
           rows="4"
           placeholder="Your feedback..."
         />
         <button
           onClick={() => setIsOpen(false)}
-          className="mt-4 bg-accent text-white px-4 py-2 rounded hover:bg-accent/80"
+          className="mt-4 bg-[#02a312] text-white px-4 py-2 rounded text-[#24292f]/70 hover:bg-[#02a312]/70 hover:text-accent"
         >
           Submit
         </button>
